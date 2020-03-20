3848f1a37698fc729cec9f42848c3cd5c429d928
lily <1183336813@qq.com>
2020-03-20 11:40:40 +0800

    第2次提交

---
+++ src/todolist/todo-tab.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>
+      <span @click='change(0)'>全部</span>
+      <span @click='change(1)'>已完成</span>
+      <span @click='change(2)'>未完成</span>
+  </div>
+</template>
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+ methods:{
+   ...mapMutations(['todolist/changeAll']),
+   change(index){
+     this['todolist/changeAll'](index)
+   }
+ }
+}
+</script>
